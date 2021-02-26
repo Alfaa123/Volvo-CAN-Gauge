d5517581f07ca84ba57c7e488c52a653ce27c4de
Alexander Elder <33436918+Alfaa123@users.noreply.github.com>
2021-02-26 00:29:48 -0800

    Testing and Bug Fixes for Due Port

--- Volvo-CAN-Gauge.ino
+++ Volvo-CAN-Gauge.ino
@@ -13,7 +13,6 @@ CAN_FRAME VHS;
 
 int x, Brightness;
 unsigned char len = 0, flagRecv = 0, Page = 0, Index = 0;
-unsigned char buf[8];
 bool NightMode, Ignition;
 static long updatePeriod = millis();
 static int gaugeAddVal = 1;
@@ -25,7 +24,8 @@ Genie genie;
 void setup()
 {
   Serial.begin(115200);
-  //Serial1.begin(115200);  // Serial1 @ 200000 (200K) BaudNuu
+  //SerialUSB.begin(115200);  // Serial1 @ 200000 (200K) BaudNuu
+  
   genie.Begin(Serial);   // Use Serial1 for talking to the Genie Library, and to the 4D Systems display
    pinMode(RESETLINE, OUTPUT);  // Set D4 on Arduino to Output (4D Arduino Adaptor V2 - Display Reset)
   digitalWrite(RESETLINE, 0);  // Reset the Display via D4
@@ -35,11 +35,13 @@ void setup()
   //Can1.begin(CAN_BPS_125K);
   CanFrames();
   delay (5000); 
+  //while(!SerialUSB);
+  //SerialUSB.print("Test");
   // Masks and filters setup for the can interface. We use these to keep resource usage low by only looking for traffic we care about.
-  Can0.setRXFilter(0x00400021, 0xFFFFFFFF, 1);
-  Can0.setRXFilter(0x19E00006, 0xFFFFFFFF, 1);
-  Can0.setRXFilter(0x0100082C, 0xFFFFFFFF, 1);
-  Can0.setRXFilter(0x19000026, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(0,0x00400021, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(1,0x19E00006, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(2,0x0100082C, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(3,0x19000026, 0xFFFFFFFF, 1);
   genie.WriteContrast(0);
 }
 
@@ -53,7 +55,7 @@ void loop()    //The main loop sends all the various CAN messages to the ECU so 
     UpdateDisplay();
   }
   x++;
-  if (x > 1 && (Page==0)){
+  if (x > 10 && (Page==0)){
      Can0.sendFrame(BP);
      x = 0;
   }
@@ -96,7 +98,7 @@ void UpdateDisplay() {               //This function takes the data retrieved in
       gaugeCurrentValue += gaugeAddVal;
       genie.WriteObject(GENIE_OBJ_IANGULAR_METER, 1, gaugeCurrentValue);
       genie.WriteObject(GENIE_OBJ_ILED_DIGITS, 1, gaugeCurrentValue);
-      updatePeriod = millis() + 2;
+      updatePeriod = millis() + 0;
     }
   }
   else if (Page == 2){
@@ -108,7 +110,7 @@ void UpdateDisplay() {               //This function takes the data retrieved in
       gaugeCurrentValue += gaugeAddVal;
       genie.WriteObject(GENIE_OBJ_IANGULAR_METER, 2, gaugeCurrentValue);
       genie.WriteObject(GENIE_OBJ_ILED_DIGITS, 2, gaugeCurrentValue);
-      updatePeriod = millis() + 2;
+      updatePeriod = millis() + 0;
     }
   }
     else if (Page == 3){
@@ -133,16 +135,23 @@ void UpdateBrightness() {                       //This function simply updates t
 }
 
 void PowerSaveLoop(){                          // This is where we come if the ignition message goes low. We loop here indefinitely until we sense the ignition has come back on. OLED is off and filters are activated to ignore any traffic except the ignition status broadcast.
+  Can0.setRXFilter(0,0x00000000, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(1,0x19E00006, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(2,0x00000000, 0xFFFFFFFF, 1);
+  Can0.setRXFilter(3,0x00000000, 0xFFFFFFFF, 1);
     while (1 == 1) {
-      delay(2000);
+      delay(200);
       if (Can0.rx_avail())           
       {
         CAN_FRAME INCOMING;
         Can0.get_rx_buff(INCOMING);
-        unsigned long canId = INCOMING.id;
-        if (canId == 0x19E00006) {
-          if ((buf[6] & B01000000) != Ignition) {
-            Ignition = (buf[6] & B01000000);
+        if (INCOMING.id == 0x19E00006) {
+          if ((INCOMING.data.bytes[6] & B01000000) != Ignition) {
+            Ignition = (INCOMING.data.bytes[6] & B01000000);
+              Can0.setRXFilter(0,0x00400021, 0xFFFFFFFF, 1);
+              Can0.setRXFilter(1,0x19E00006, 0xFFFFFFFF, 1);
+              Can0.setRXFilter(2,0x0100082C, 0xFFFFFFFF, 1);
+              Can0.setRXFilter(3,0x19000026, 0xFFFFFFFF, 1);
             UpdateIgnition();
             break;
           }
@@ -156,34 +165,33 @@ void MessageRecieveLoop(){                                                  //I 
   {
    CAN_FRAME INCOMING;
    Can0.get_rx_buff(INCOMING);
-
-    unsigned long canId = INCOMING.id;
-    if (canId == 0x00400021) {
-      if (buf[4] == 0x9d) {
-        Boost = buf[5];
+   //SerialUSB.println(INCOMING.id,HEX);
+    if (INCOMING.id == 0x00400021) {
+      if (INCOMING.data.bytes[4] == 0x9d) {
+        Boost = INCOMING.data.bytes[5];
       }
-      if (buf[4] == 0xd8) {
-        CoolantTemp = buf[5];
+      if (INCOMING.data.bytes[4] == 0xd8) {
+        CoolantTemp = INCOMING.data.bytes[5];
         CoolantTemp = CoolantTemp*0.75-48;
       }
-      if (buf[4] == 0xCE) {
-        IntakeTemp = buf[5];
+      if (INCOMING.data.bytes[4] == 0xCE) {
+        IntakeTemp = INCOMING.data.bytes[5];
         IntakeTemp = IntakeTemp*0.75-48;
         IntakeTemp = IntakeTemp*1.8+32;
       }
-      if (buf[4] == 0x36) {
-        IgnitionAngle = buf[5];
+      if (INCOMING.data.bytes[4] == 0x36) {
+        IgnitionAngle = INCOMING.data.bytes[5];
         IgnitionAngle = IgnitionAngle*191.25/255;
       }
     }
-    if (canId == 0x19E00006) {
-      if ((buf[6] & B01000000) == !Ignition) {
-        Ignition = (buf[6] & B01000000);
+    if (INCOMING.id == 0x19E00006) {
+      if ((INCOMING.data.bytes[6] & B01000000) == !Ignition) {
+        Ignition = (INCOMING.data.bytes[6] & B01000000);
         UpdateIgnition();
       }
     }
-    if (canId == 0x19000026){
-      if (buf[7] && B00100000){
+    if (INCOMING.id == 0x19000026){
+      if (INCOMING.data.bytes[7] && B00100000){
          if (ButtonHeld < 1){
            ButtonHeld = millis();
         }
@@ -200,16 +208,16 @@ void MessageRecieveLoop(){                                                  //I 
         ButtonHeld = 0;
       }
       }
-    if (canId == 0x0100082C) {
-      if ((buf[1] & B10000000) != NightMode) {
-        NightMode = buf[1] & B10000000;
+    if (INCOMING.id == 0x0100082C) {
+      if ((INCOMING.data.bytes[1] & B10000000) != NightMode) {
+        NightMode = INCOMING.data.bytes[1] & B10000000;
         if (!NightMode) {
           Brightness = Brightness * 0.8;
         }
         UpdateBrightness();
       }
-      if ((buf[0] & B00001111) != Brightness) {
-        Brightness = (buf[0] & B00001111) * 15;
+      if ((INCOMING.data.bytes[0] & B00001111) != Brightness) {
+        Brightness = (INCOMING.data.bytes[0] & B00001111) * 15;
         if (!NightMode) {
           Brightness = Brightness * 0.8;
         }
@@ -249,70 +257,70 @@ void UpdateIgnition() {               //This is where we go if the ignition stat
 
 void CanFrames()
 {
-BP.id = 0x000FFFFE;
 BP.extended = 1;
+BP.id = 0x000FFFFE;
 BP.length = 8;
-BP.data.bytes[1]=0xCD;
-BP.data.bytes[2]=0x7a;
-BP.data.bytes[3]=0xa6;
-BP.data.bytes[4]=0x12;
-BP.data.bytes[5]=0x9d;
-BP.data.bytes[6]=0x01;
+BP.data.bytes[0]=0xCD;
+BP.data.bytes[1]=0x7a;
+BP.data.bytes[2]=0xa6;
+BP.data.bytes[3]=0x12;
+BP.data.bytes[4]=0x9d;
+BP.data.bytes[5]=0x01;
+BP.data.bytes[6]=0x00;
 BP.data.bytes[7]=0x00;
-BP.data.bytes[8]=0x00;
-RPM.id = 0x000FFFFE;
 RPM.extended = 1;
+RPM.id = 0x000FFFFE;
 RPM.length = 8;
-RPM.data.bytes[1]=0xCD;
-RPM.data.bytes[2]=0x7a;
-RPM.data.bytes[3]=0xa6;
-RPM.data.bytes[4]=0x10;
-RPM.data.bytes[5]=0x1d;
-RPM.data.bytes[6]=0x01;
+RPM.data.bytes[0]=0xCD;
+RPM.data.bytes[1]=0x7a;
+RPM.data.bytes[2]=0xa6;
+RPM.data.bytes[3]=0x10;
+RPM.data.bytes[4]=0x1d;
+RPM.data.bytes[5]=0x01;
+RPM.data.bytes[6]=0x00;
 RPM.data.bytes[7]=0x00;
-RPM.data.bytes[8]=0x00;
 COL.id = 0x000FFFFE;
 COL.extended = 1;
 COL.length = 8;
-COL.data.bytes[1]=0xCD;
-COL.data.bytes[2]=0x7a;
-COL.data.bytes[3]=0xa6;
-COL.data.bytes[4]=0x10;
-COL.data.bytes[5]=0xd8;
-COL.data.bytes[6]=0x01;
+COL.data.bytes[0]=0xCD;
+COL.data.bytes[1]=0x7a;
+COL.data.bytes[2]=0xa6;
+COL.data.bytes[3]=0x10;
+COL.data.bytes[4]=0xd8;
+COL.data.bytes[5]=0x01;
+COL.data.bytes[6]=0x00;
 COL.data.bytes[7]=0x00;
-COL.data.bytes[8]=0x00;
-IAT.id = 0x000FFFFE;
 IAT.extended = 1;
+IAT.id = 0x000FFFFE;
 IAT.length = 8;
-IAT.data.bytes[1]=0xCD;
-IAT.data.bytes[2]=0x7a;
-IAT.data.bytes[3]=0xa6;
-IAT.data.bytes[4]=0x10;
-IAT.data.bytes[5]=0xce;
-IAT.data.bytes[6]=0x01;
+IAT.data.bytes[0]=0xCD;
+IAT.data.bytes[1]=0x7a;
+IAT.data.bytes[2]=0xa6;
+IAT.data.bytes[3]=0x10;
+IAT.data.bytes[4]=0xce;
+IAT.data.bytes[5]=0x01;
+IAT.data.bytes[6]=0x00;
 IAT.data.bytes[7]=0x00;
-IAT.data.bytes[8]=0x00;
-IA.id = 0x000FFFFE;
 IA.extended = 1;
+IA.id = 0x000FFFFE;
 IA.length = 8;
-IA.data.bytes[1]=0xCD;
-IA.data.bytes[2]=0x7a;
-IA.data.bytes[3]=0xa6;
-IA.data.bytes[4]=0x10;
-IA.data.bytes[5]=0x36;
-IA.data.bytes[6]=0x01;
+IA.data.bytes[0]=0xCD;
+IA.data.bytes[1]=0x7a;
+IA.data.bytes[2]=0xa6;
+IA.data.bytes[3]=0x10;
+IA.data.bytes[4]=0x36;
+IA.data.bytes[5]=0x01;
+IA.data.bytes[6]=0x00;
 IA.data.bytes[7]=0x00;
-IA.data.bytes[8]=0x00;
-VHS.id = 0x000FFFFE;
 VHS.extended = 1;
+VHS.id = 0x000FFFFE;
 VHS.length = 8;
-VHS.data.bytes[1]=0xCD;
-VHS.data.bytes[2]=0x7a;
-VHS.data.bytes[3]=0xa6;
-VHS.data.bytes[4]=0x11;
-VHS.data.bytes[5]=0x40;
-VHS.data.bytes[6]=0x01;
+VHS.data.bytes[0]=0xCD;
+VHS.data.bytes[1]=0x7a;
+VHS.data.bytes[2]=0xa6;
+VHS.data.bytes[3]=0x11;
+VHS.data.bytes[4]=0x40;
+VHS.data.bytes[5]=0x01;
+VHS.data.bytes[6]=0x00;
 VHS.data.bytes[7]=0x00;
-VHS.data.bytes[8]=0x00;
 }
